7af75a53b013f330f5b6f9d0f266c06533a22b98
darkcack <57454282+darkcack@users.noreply.github.com>
2020-12-19 04:52:14 +0300

    Added Homework4.cpp

---
+++ Homework4.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<clocale>
+#include<locale.h>
+#define NODE 20
+using namespace std;
+int check =0;
+
+int graph[NODE][NODE] =
+{
+
+
+
+    {0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+    {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+};
+
+bool isCycle(int u, bool visited[], int parent) {
+   visited[u] = true;    //mark v as visited
+   for(int v = 0; v<NODE; v++) {
+      if(graph[u][v]) {
+         if(!visited[v]) {     //when the adjacent node v is not visited
+            if(isCycle(v, visited, u)) {
+               return true;
+            }
+         } else if(v != parent) {    //when adjacent vertex is visited but not parent
+            return true;    //there is a cycle
+         }
+      }
+   }
+   return false;
+}
+
+bool isTree() {
+   bool *vis = new bool[NODE];
+
+   for(int i = 0; i<NODE; i++)
+      vis[i] = false;    //initialize as no node is visited
+
+   if(isCycle(0, vis, -1))
+    {
+        check =1; // döngü var mý yok mu
+     //check if there is a cycle or not
+      return false;
+    }
+
+   for(int i = 0; i<NODE; i++) {
+      if(!vis[i]){
+        check =2; // tüm dügümler bagli mi
+        return false;
+      }    //if there is a node, not visited by traversal, graph is not connected
+
+   }
+
+
+   return true;
+}
+
+int main() {
+  	setlocale(LC_ALL,"Turkish");
+   if(isTree())
+      cout << "Verilen graf bir agactir.";
+   else if(check==1)
+    {
+     cout << "Verilen graf bir agac degildir. Cunku dongu barindiriyor.";
+    }
+   else if(check==2)
+    {
+    cout << "Verilen graf bir agac degildir. Cunku grafta bagli olmayan dugumler var.";
+
+    }
+
+}
